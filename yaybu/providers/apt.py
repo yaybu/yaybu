64d8719ee2b484dba18adbce17fcc2a8a8dd9f5b
John Carr <john.carr@isotoma.com>
2011-02-16 13:50:34 +0000

    Remove imports not needed

--- yaybu/providers/apt.py
+++ yaybu/providers/apt.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import shlex
-
 from yaybu.core import provider
 from yaybu import resources
 
