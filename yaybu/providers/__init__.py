782024a6e9120251bfdc808d2fb4f1cecd197723
John Carr <john.carr@isotoma.com>
2011-02-16 13:00:09 +0000

    Stub Apt provider

--- yaybu/providers/__init__.py
+++ yaybu/providers/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from yaybu.providers import (
+    apt,
     execute,
     filesystem,
     subversion,
---
+++ yaybu/providers/apt.py
@@ -0,0 +1,35 @@
+# Copyright 2011 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shlex
+
+from yaybu.core import provider
+from yaybu import resources
+
+class Apt(provider.Provider):
+
+    policies = (resources.package.PackageInstallPolicy,)
+
+    @classmethod
+    def isvalid(self, *args, **kwargs):
+        return super(Execute, self).isvalid(*args, **kwargs)
+
+    def apply(self, shell):
+        command = ["apt-get", "install", "-y", self.resource.name]
+        returncode, stdout, stderr = shell.execute(command)
+
+        if returncode != 0:
+            raise RuntimeError("%s failed with return code %d" % (self.resource, expected_returncode))
+
