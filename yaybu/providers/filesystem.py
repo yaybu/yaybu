74acd7211604ca5edb1a3058ba5d9f6ced867cca
John Carr <john.carr@isotoma.com>
2011-02-16 13:27:51 +0000

    Add stub directory creator

--- yaybu/providers/filesystem.py
+++ yaybu/providers/filesystem.py
@@ -82,3 +82,46 @@ class File(provider.Provider):
         if self.resource.mode is not None:
             if mode != self.resource.mode:
                 shell.execute(["chmod", "%o" % self.resource.mode, name])
+
+
+class Directory(provider.Provider):
+
+    policies = (resources.filesystem.DirectoryCreatedPolicy,)
+
+    @classmethod
+    def isvalid(self, *args, **kwargs):
+        return super(Directory, self).isvalid(*args, **kwargs)
+
+    def apply(self, shell):
+        name = self.resource.name
+        exists = False
+        uid = None
+        gid=None
+        mode=None
+
+        if os.path.exists(name):
+            exists = True
+            st = os.stat(name)
+            uid = st.st_uid
+            gid = st.st_gid
+            mode = st.st_mode
+            if mode > 32767:
+                mode = mode - 32768
+
+        if not exists:
+            shell.execute(["mkdir", name])
+
+        if self.resource.owner is not None:
+            owner = pwd.getpwnam(self.resource.owner)
+            if owner.pw_uid != uid:
+                shell.execute(["chown", self.resource.owner, name])
+
+        if self.resource.group is not None:
+            group = grp.getgrnam(self.resource.group)
+            if group.gr_gid != gid:
+                shell.execute(["chgrp", self.resource.group, name])
+
+        if self.resource.mode is not None:
+            if mode != self.resource.mode:
+                shell.execute(["chmod", "%o" % self.resource.mode, name])
+
