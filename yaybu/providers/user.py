8c50a5176d3814236a1745fc20a937b597797997
John Carr <john.carr@isotoma.com>
2011-02-20 11:54:25 +0000

    User resource/provider stubs

---
+++ yaybu/providers/user.py
@@ -0,0 +1,43 @@
+# Copyright 2011 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shlex
+
+from yaybu.core import provider
+from yaybu.core import error
+from yaybu import resources
+
+import logging
+
+logger = logging.getLogger("provider")
+
+class User(provider.Provider):
+
+    policies = (resources.user.UserApplyPolicy,)
+
+    @classmethod
+    def isvalid(self, *args, **kwargs):
+        return super(User, self).isvalid(*args, **kwargs)
+
+    def apply(self, shell):
+        command = []
+
+
+
+        returncode, stdout, stderr = shell.execute(command)
+        if returncode != 0:
+            raise error.ExecutionError("%s failed with return code %d" % (self.resource, returncode))
+
+
