9e5d6eb19334cf1538a3c491b8ddc68bab3e48aa
John Carr <john.carr@isotoma.com>
2011-03-29 15:08:34 +0100

    Add a test that uses a new group after creating it

--- yaybutest/providers/group/test_group.py
+++ yaybutest/providers/group/test_group.py
@@ -52,6 +52,21 @@ class TestGroup(TestCase):
         self.failUnlessEqual(rv, 4)
         self.failUnlessRaises(KeyError, self.get_group, "test")
 
+    def test_add_group_and_use_it(self):
+        self.check_apply("""
+            resources:
+                - Group:
+                    name: test
+                - File:
+                    name: /etc/test
+                    group: test
+                - Execute:
+                    name: test-group
+                    command: python -c "import os, grp; open('/etc/test2', 'w').write(grp.getgrgid(os.getgid()).gr_name)"
+                    group: test
+            """)
+        self.failUnlessEqual(open(self.enpathinate("/etc/test2")).read(), "test")
+
 
 class TestGroupRemove(TestCase):
 
