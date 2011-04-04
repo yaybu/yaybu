1c715099146c486ce1cdcb5cbe35ad5063e5b19d
Alex Holmes <alex@alex-holmes.com>
2011-04-04 01:12:26 +0100

    Add a git provider
     * Doesn't yet deal with change in repository variable
     * Updated subversion provider to act as default provider
       for the CheckoutSyncPolicy if no scm is explicitly
       specified in the configuration.

---
+++ yaybu/providers/git.py
@@ -0,0 +1,98 @@
+# Copyright 2011 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, logging
+
+from yaybu.core.provider import Provider
+from yaybu import resources
+
+log = logging.getLogger("git")
+
+class Git(Provider):
+
+    policies = (resources.checkout.CheckoutSyncPolicy,)
+
+    REMOTE_NAME = "origin"
+
+    @classmethod
+    def isvalid(self, policy, resource, yay):
+        return resource.scm_name and resource.scm_name.lower() == "git"
+
+    def git(self, context, action, *args, **kwargs):
+        command = [
+            "git",
+            "--git-dir=%s" % os.path.join(self.resource.name, ".git"),
+            "--work-tree=%s" % self.resource.name,
+            "--no-pager",
+            action,
+        ]
+
+        command.extend(list(args))
+
+        return context.shell.execute(command, user=self.resource.user)
+
+    def action_clone(self, context):
+        """Adds resource.repository as a remote, but unlike a
+        typical clone, does not check it out
+
+        """
+        if not os.path.exists(os.path.join(self.resource.name, ".git")):
+            self.git(context, "init", self.resource.name)
+            git_parameters = [
+                "remote", "add",
+                self.REMOTE_NAME,
+                self.resource.repository,
+            ]
+
+            # If necessary, only fetch the branch we need
+            if self.resource.branch:
+                git_parameters.extend([
+                    "-t", self.resource.branch,
+                    "-m", "self.resource.branch",
+                ])
+
+            self.git(context, *git_parameters)
+
+            return True
+        else:
+            return False
+
+    def action_checkout(self, context):
+        # Revision takes precedent over branch
+        if self.resource.revision:
+            newref = self.resource.revision
+        elif self.resource.branch:
+            newref = "%s/%s" % (self.REMOTE_NAME, self.resource.branch)
+        else:
+            raise ParseError("You must specify either a revision or a branch")
+
+        # check to see if anything has changed
+        returncode, stdout, stderr = self.git(context, "diff", "--shortstat", newref)
+        if stdout.strip() == "":
+            return False
+        else:
+            self.git(context, "checkout", newref)
+            return True
+
+    def apply(self, context):
+        log.info("Syncing %s" % self.resource)
+
+        # If necessary, clone the repository
+        if not os.path.exists(os.path.join(self.resource.name, ".git")):
+            self.action_clone(context)
+
+        # Always update the remotes
+        self.git(context, "remote", "update")
+
+        return self.action_checkout(context)
