5716a74c540bf07cfbf879279a8889b234c46767
Alex Holmes <alex@alex-holmes.com>
2011-07-22 12:17:51 +0100

    Add some tests for checking out tags.

--- yaybu/providers/tests/test_git.py
+++ yaybu/providers/tests/test_git.py
@@ -67,6 +67,59 @@ class GitTest(FakeChrootTestCase):
             }
         )
 
+    def test_checkout_tag(self):
+        """Test checking out a particular tag, when specifying the tag within
+        the branch field in the Checkout mapping"""
+
+        CLONED_REPO = "/tmp/test_checkout_tag"
+        TAG = "0.1.0"
+
+        self.fixture.check_apply("""
+            resources:
+                - Checkout:
+                    scm: git
+                    name: %(clone_dir)s
+                    repository: %(repo_url)s
+                    branch: %(tag)s
+            """ % {
+                "clone_dir": CLONED_REPO,
+                "repo_url": self.UPSTREAM_REPO_2,
+                "tag": TAG,
+            }
+        )
+
+
+    def test_branch_to_tag(self):
+        """Test checking out a branch, then changing to a tag, then back again"""
+
+        CLONED_REPO = "/tmp/test_branch_to_tag"
+        TAG = "0.1.0"
+        BRANCH = "master"
+
+        framework = """
+            resources:
+                - Checkout:
+                    scm: git
+                    name: %(clone_dir)s
+                    repository: %(repo_url)s
+                    branch: %(branch_or_tag)s
+            """
+
+        branch = framework % {
+            "clone_dir": CLONED_REPO,
+            "repo_url": self.UPSTREAM_REPO_2,
+            "branch_or_tag": BRANCH,
+        }
+
+        tag = framework % {
+            "clone_dir": CLONED_REPO,
+            "repo_url": self.UPSTREAM_REPO_2,
+            "branch_or_tag": TAG,
+        }
+
+        self.fixture.check_apply(branch)
+        self.fixture.check_apply(tag)
+        self.fixture.check_apply(branch)
 
     def test_change_repo(self):
         """Test for the edge-case where a different repository must be checked
