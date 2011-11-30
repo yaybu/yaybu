fdb7abb09f68ea771fed148fec7fab226f463a27
John Carr <john.carr@isotoma.com>
2011-11-30 00:01:50 +0000

    Better bootstrap defaults than upstream

--- bootstrap.py
+++ bootstrap.py
@@ -101,10 +101,11 @@ local resources, you can keep this script from going over the network.
 
 parser = OptionParser(usage=usage)
 parser.add_option("-v", "--version", dest="version",
-                          help="use a specific zc.buildout version")
+                          help="use a specific zc.buildout version", default="1.4.3")
 parser.add_option("-d", "--distribute",
-                   action="store_true", dest="use_distribute", default=False,
+                   action="store_true", dest="use_distribute", default=True,
                    help="Use Distribute rather than Setuptools.")
+parser.add_option("--distribute-version", dest="distribute_version", default="0.6.21")
 parser.add_option("--setup-source", action="callback", dest="setup_source",
                   callback=normalize_to_url, nargs=1, type="string",
                   help=("Specify a URL or file location for the setup file. "
@@ -169,6 +170,8 @@ except ImportError:
         setup_args['download_base'] = options.download_base
     if options.use_distribute:
         setup_args['no_fake'] = True
+    if options.distribute_version:
+        setup_args['version'] = options.distribute_version
     ez['use_setuptools'](**setup_args)
     if 'pkg_resources' in sys.modules:
         reload(sys.modules['pkg_resources'])
