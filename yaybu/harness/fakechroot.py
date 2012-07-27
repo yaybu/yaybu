c7ea385850d50148365ffaa930c617ae234b7773
John Carr <john.carr@isotoma.com>
2012-07-27 05:18:08 +0100

    Update integration environment with build-essential

--- yaybu/harness/fakechroot.py
+++ yaybu/harness/fakechroot.py
@@ -132,7 +132,7 @@ class FakeChrootFixture(Fixture):
     def build_environment(self):
         distro = self.default_distro()
         commands = [
-            "fakeroot fakechroot -s debootstrap --variant=fakechroot --include=git-core,python-setuptools,python-dateutil,python-magic,ubuntu-keyring,gpgv %(distro)s %(base_image)s",
+            "fakeroot fakechroot -s debootstrap --variant=fakechroot --include=git-core,python-setuptools,python-dateutil,python-magic,ubuntu-keyring,gpgv,build-essential %(distro)s %(base_image)s",
             "fakeroot fakechroot -s /usr/sbin/chroot %(base_image)s apt-get update",
             ]
         if not os.path.exists(self.testbase):
