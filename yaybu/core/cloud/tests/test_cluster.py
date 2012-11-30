82df0dafe5bb0c29153fbe310aea0b603b9fafd7
John Carr <john.carr@isotoma.com>
2012-11-30 00:01:41 +0000

    Test the cluster object from the command line API

---
+++ yaybu/core/cloud/tests/test_cluster.py
@@ -0,0 +1,80 @@
+import testtools
+import os
+import tempfile
+
+from yaybu.core.cloud.part import Part
+from yaybu.core.command import YaybuCmd
+
+
+class PartTester(Part):
+
+    provisioned = False
+
+    @classmethod
+    def create_from_yay_expression(cls, cluster, name, v):
+        return cls(cluster, name)
+
+    def decorate_config(self, config):
+        pass
+
+    def instantiate(self):
+        pass
+
+    def provision(self):
+        self.provisioned = True
+        self.cluster.make_context().get_config().mapping.get("parts").get(self.name).get("somevar").resolve()
+
+
+class TestClusterIntegration(testtools.TestCase):
+
+    """
+    Exercises the cluster via the command line interface
+    """
+
+    def _config(self, contents):
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.write(contents)
+        f.close()
+        path = os.path.realpath(f.name)
+        self.addCleanup(os.unlink, path)
+        return path       
+
+    def _provision(self, clustername, config):
+        cmd = YaybuCmd()
+        return cmd.onecmd("provision %s %s" % (clustername, self._config(config)))
+
+    def test_empty_cluster(self):
+        self._provision("test", """
+            parts: {}
+            """)
+
+    def test_single_node_cluster(self):
+        self._provision("test", """
+            parts:
+              node1:
+                class: parttester
+                somevar: hello
+            """)
+
+    def test_multi_node_cluster(self):
+        self._provision("test", """
+            parts:
+              node1:
+                class: parttester
+                somevar: hello
+              node2:
+                class: parttester
+                somevar: hello
+            """)
+
+    def test_multi_node_cluster_with_dependency(self):
+        self._provision("test", """
+            parts:
+              node1:
+                class: parttester
+                somevar: hello
+              node2:
+                class: parttester
+                somevar: ${parts.node1.hello}
+            """)
+
