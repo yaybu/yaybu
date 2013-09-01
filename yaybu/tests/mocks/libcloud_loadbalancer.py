96f0641aeb8e21b690ad721a6dce06cb8a4e7db3
John Carr <john.carr@unrouted.co.uk>
2013-09-01 18:13:03 +0100

    Put loadbalancer mock somewhere more reusable

---
+++ yaybu/tests/mocks/libcloud_loadbalancer.py
@@ -0,0 +1,82 @@
+
+from libcloud.loadbalancer.types import State, LibcloudLBError
+from libcloud.loadbalancer.base import Driver, LoadBalancer as LB, Member, Algorithm
+
+from yaybu.loadbalancer import LoadBalancer
+
+
+class MockLoadBalancer(Driver):
+    name = 'dummy'
+
+    next_id = 0
+    balancers = {}
+
+    @classmethod
+    def install(cls, testcase):
+        testcase.addCleanup(setattr, MockLoadBalancer, "next_id", 0)
+        testcase.addCleanup(setattr, MockLoadBalancer, "members", {})
+
+        LoadBalancer.extra_drivers['DUMMY'] = MockLoadBalancer
+        testcase.addCleanup(LoadBalancer.extra_drivers.pop, 'DUMMY', None)
+
+    def __init__(self, key, secret):
+        # Need a constructor so HTTP .connect() isn't invoked in base class
+        pass
+
+    def list_protocols(self):
+        return ['tcp', 'ssl', 'http', 'https']
+
+    def list_supported_algorithms(self):
+        return [Algorithm.RANDOM]
+
+    def list_balancers(self):
+        return self.balancers.values()
+
+    def create_balancer(self, name, port, protocol, algorithm, members):
+        pending_balancer = LB(
+            id=str(self.next_id),
+            name=name,
+            state=State.PENDING,
+            ip="192.168.1.2",
+            port=port,
+            driver=self
+        )
+        balancer = LB(
+            id=str(self.next_id),
+            name=name,
+            state=State.RUNNING,
+            ip="192.168.1.2",
+            port=port,
+            driver=self
+        )
+        pending_balancer._members = []
+        balancer._members = []
+        self.balancers[str(self.next_id)] = balancer
+        self.next_id += 1
+        return pending_balancer
+
+    def destroy_balancer(self, balancer):
+        if not balancer.id in self.balancers.keys():
+            raise LibcloudLBError("Balancer does not exist")
+        del self.balancers[balancer.id]
+        return True
+
+    def get_balancer(self, balancer_id):
+        if not balancer_id in self.balancers:
+            raise LibcloudLBError("Balancer does not exist")
+        return self.balancers[balancer_id]
+
+    def balancer_attach_member(self, balancer, member):
+        b = self.get_balancer(balancer.id)
+        m = Member(member.id, member.ip, member.port, balancer=b)
+        b._members.append(m)
+        return m
+
+    def balancer_detach_member(self, balancer, member):
+        b = self.get_balancer(balancer.id)
+        b._members = [m for m in balancer._members if m.id != member.id]
+        return True
+
+    def balancer_list_members(self, balancer):
+        return list(balancer._members)
+
--- yaybu/tests/test_loadbalancer.py
+++ yaybu/tests/test_loadbalancer.py
@@ -1,19 +1,12 @@
 
-from libcloud.loadbalancer.types import State, LibcloudLBError
-from libcloud.loadbalancer.base import Driver, LoadBalancer as LB, Member, Algorithm
-
-from yaybu.loadbalancer import LoadBalancer
 from yaybu.tests.base import TestCase
+from yaybu.tests.mocks.libcloud_loadbalancer import MockLoadBalancer
 
 
 class TestLoadBalancer(TestCase):
 
     def setUp(self):
-        self.addCleanup(setattr, MockLoadBalancer, "next_id", 0)
-        self.addCleanup(setattr, MockLoadBalancer, "members", {})
-
-        LoadBalancer.extra_drivers['DUMMY'] = MockLoadBalancer
-        self.addCleanup(LoadBalancer.extra_drivers.pop, 'DUMMY', None)
+        MockLoadBalancer.install(self)
 
     def test_empty_records_list(self):
         self.up("""
@@ -31,71 +24,4 @@ class TestLoadBalancer(TestCase):
                 members: []
             """)
 
-
-class MockLoadBalancer(Driver):
-    name = 'dummy'
-
-    next_id = 0
-    balancers = {}
-
-    def __init__(self, key, secret):
-        # Need a constructor so HTTP .connect() isn't invoked in base class
-        pass
-
-    def list_protocols(self):
-        return ['tcp', 'ssl', 'http', 'https']
-
-    def list_supported_algorithms(self):
-        return [Algorithm.RANDOM]
-
-    def list_balancers(self):
-        return self.balancers.values()
-
-    def create_balancer(self, name, port, protocol, algorithm, members):
-        pending_balancer = LB(
-            id=str(self.next_id),
-            name=name,
-            state=State.PENDING,
-            ip="192.168.1.2",
-            port=port,
-            driver=self
-        )
-        balancer = LB(
-            id=str(self.next_id),
-            name=name,
-            state=State.RUNNING,
-            ip="192.168.1.2",
-            port=port,
-            driver=self
-        )
-        pending_balancer._members = []
-        balancer._members = []
-        self.balancers[str(self.next_id)] = balancer
-        self.next_id += 1
-        return pending_balancer
-
-    def destroy_balancer(self, balancer):
-        if not balancer.id in self.balancers.keys():
-            raise LibcloudLBError("Balancer does not exist")
-        del self.balancers[balancer.id]
-        return True
-
-    def get_balancer(self, balancer_id):
-        if not balancer_id in self.balancers:
-            raise LibcloudLBError("Balancer does not exist")
-        return self.balancers[balancer_id]
-
-    def balancer_attach_member(self, balancer, member):
-        b = self.get_balancer(balancer.id)
-        m = Member(member.id, member.ip, member.port, balancer=b)
-        b._members.append(m)
-        return m
-
-    def balancer_detach_member(self, balancer, member):
-        b = self.get_balancer(balancer.id)
-        b._members = [m for m in balancer._members if m.id != member.id]
-        return True
-
-    def balancer_list_members(self, balancer):
-        return list(balancer._members)
 
