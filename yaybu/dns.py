8e006cf008e8f7b002a9db40f63d172b9449402e
John Carr <john.carr@isotoma.com>
2013-09-02 14:36:57 +0100

    Add missing headers

--- yaybu/dns.py
+++ yaybu/dns.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Isotoma Limited
+# Copyright 2013 Isotoma Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
