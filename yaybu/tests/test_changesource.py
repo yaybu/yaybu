8edda5dfcb14841b48a0178f7c870b949950a9ce
John Carr <john.carr@isotoma.com>
2013-09-14 23:56:28 +0100

    Don't depend on an external git repo to test GitChangeSource

--- yaybu/tests/test_changesource.py
+++ yaybu/tests/test_changesource.py
@@ -12,18 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import mock
+
 from libcloud.dns.types import RecordType
 
 from yaybu import error
 from yaybu.tests.base import TestCase
 
+GIT_LS_REMOTE = """
+e8f51afcbfa78b077b5e91797cdd77c35350122f    HEAD
+e8f51afcbfa78b077b5e91797cdd77c35350122f    refs/heads/master
+352f0dcc87e6f64d9ae83cae3bf696b8c69a3987    refs/tags/3.0
+b33e819fbdf0115f6e367d79716c1c1633afd00b    refs/tags/3.0^{}
+""".strip()
+
 
 class TestChangeSource(TestCase):
 
+    def setUp(self):
+        patcher = mock.patch("yaybu.changesource.subprocess")
+        self.subprocess = patcher.start()
+        self.addCleanup(patcher.stop)
+        self.popen = self.subprocess.Popen
+
     def test_get_current_branch(self):
+        self.popen.return_value.communicate.return_value = [GIT_LS_REMOTE, ""]
+
         self.assertRaises(error.NothingChanged, self.up, """
             new GitChangeSource as mychangesource:
-               repository: .
+               repository: /nowhere
                polling-interval: 0
 
             new Printer as myprinter:
