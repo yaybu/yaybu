ed7d8fa7b8cb93f5dd00a82981079c32b2ef4148
John Carr <john.carr@isotoma.com>
2013-09-15 15:35:10 +0100

    Make the subversion tests much more useful

--- yaybu/tests/test_provisioner_providers_subversion.py
+++ yaybu/tests/test_provisioner_providers_subversion.py
@@ -16,15 +16,66 @@ from yaybu.tests.provisioner_fixture import TestCase
 from yaybu.core.error import MissingDependency
 
 
-class SubversionMissingTest(TestCase):
+class TestSubversion(TestCase):
 
     def test_missing_svn(self):
         self.assertRaises(MissingDependency, self.apply, """
            resources:
+               - Package:
+                   name: subversion
+                   policy: uninstall
+
                - Checkout:
                    scm: subversion
                    name: /dest
                    repository: /source
                    branch: trunk
            """)
+
+    def test_checkout(self):
+        self.check_apply("""
+            resources:
+                - Checkout:
+                    scm: subversion
+                    name: /subversion
+                    repository: https://github.com/isotoma/isotoma.recipe.django
+                    branch: trunk
+            """)
+        self.failUnlessExists("/subversion/setup.py")
+
+    def test_checkout_tag(self, tag="3.0.2"):
+        self.check_apply("""
+            resources:
+                - Checkout:
+                    scm: subversion
+                    name: /subversion
+                    repository: https://github.com/isotoma/isotoma.recipe.django
+                    tag: %s
+            """ % tag)
+
+    def test_checkout_branch(self):
+        self.check_apply("""
+            resources:
+                - Checkout:
+                    scm: subversion
+                    name: /subversion
+                    repository: https://github.com/isotoma/isotoma.recipe.django
+                    branch: branches/version3
+            """)
+
+    def test_change_to_branch(self):
+        """Test change to branch after an initial checkout, and back again """
+        self.test_checkout()
+        self.test_checkout_branch()
+        self.test_checkout()
+
+    def test_change_trunk_to_tag(self):
+        """Test change to tag after an initial checkout, and back again """
+        self.test_checkout()
+        self.test_checkout_tag()
+        self.test_checkout()
+
+    def test_change_tag_to_tag(self):
+        self.test_checkout_tag()
+        self.test_checkout_tag("3.1.6")
 
