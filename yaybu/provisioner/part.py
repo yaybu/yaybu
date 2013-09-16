8b62d1d3760555feb1b13e820ac0e9f9d64a9871
John Carr <john.carr@isotoma.com>
2013-09-16 16:14:03 +0100

    Port should be specified as an integer

--- yaybu/provisioner/part.py
+++ yaybu/provisioner/part.py
@@ -54,7 +54,7 @@ class Provision(base.GraphExternalAction):
 
         self.host = hostname
         self.user = self.params.server.user.as_string(default=getpass.getuser())
-        self.port = self.params.server.port.as_string(default=22)
+        self.port = self.params.server.port.as_int(default=22)
         self.password = self.params.server.password.as_string(default="")
         self.private_key = self.params.server.private_key.as_string(default="")
 
