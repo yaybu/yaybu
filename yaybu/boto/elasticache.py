690d9306deb8d8978b2bfdc9f061662eadc8c4fe
John Carr <john.carr@isotoma.com>
2013-11-30 16:34:37 +0000

    Add throbber progress updates

--- yaybu/boto/elasticache.py
+++ yaybu/boto/elasticache.py
@@ -29,7 +29,8 @@ class CacheSecurityGroup(BotoResource):
     def create(self):
         name = self.params.name.as_string()
         description = self.params.description.as_string(default=name)
-        self.connection.create_dbsecurity_group(name, description)
+        with self.root.ui.throbber("Creating CacheSecurityGroup '%s'" % name):
+            self.connection.create_dbsecurity_group(name, description)
         return True
 
     def update(self, existing):
@@ -38,7 +39,8 @@ class CacheSecurityGroup(BotoResource):
         if existing.description != description:
             changed = True
         if changed:
-            self.connection.update_dbsecurity_group(name, description)
+            with self.root.ui.throbber("Updating CacheSecurityGroup '%s'" % name):
+                self.connection.update_dbsecurity_group(name, description)
         return changed
 
     def apply(self):
@@ -83,12 +85,14 @@ class CacheSecurityGroupIngress(BotoResource):
         if filter(lambda x: x['EC2SecurityGroupName'] == ec2_group.name, result['EC2SecurityGroups']):
             return
 
-        self.connection.authorize_cache_security_group_ingress(cache_group_name, ec2_group.name, ec2_group.owner_id)
-
-        # FIXME: Ideally wait for Status to shift from Status=='authorizing' to Status=='authorized'
+        with self.root.ui.throbber("Creating CacheSecurityGroupIngress ('%s' -> '%s')" % (ec2_group.name, cache_group_name)):
+            self.connection.authorize_cache_security_group_ingress(cache_group_name, ec2_group.name, ec2_group.owner_id)
+            # FIXME: Ideally wait for Status to shift from Status=='authorizing' to Status=='authorized'
 
 
 class CacheCluster(BotoResource):
+
+    module = boto.elasticache
 
     def create(self):
         # FIXME: Actually get theses settings from self.params
@@ -118,11 +122,11 @@ class CacheCluster(BotoResource):
             self.update(result)
 
         if result['CacheClusterStatus'] != 'available':
-            # FIXME: Add throbber here
-            while result['CacheClusterStatus'] != 'available':
-                response = self.connection.describe_cache_clusters(cache_cluster_id=id, show_cache_node_info=True)
-                result = response['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]
-                time.sleep(1)
-                # throbber.throb()
+            with self.root.ui.throbber("Waiting for CacheCluster to be in state 'available'") as throbber:
+                while result['CacheClusterStatus'] != 'available':
+                    response = self.connection.describe_cache_clusters(cache_cluster_id=id, show_cache_node_info=True)
+                    result = response['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters'][0]
+                    time.sleep(1)
+                    throbber.throb()
 
         # FIXME: Extract Endpoint and other outputs and place in graph
