8b38a5107f39de7b089f4f4cde86aa4d4e635b40
Doug Winter <doug.winter@isotoma.com>
2014-03-06 09:43:39 +0000

    fetches remote files

--- yaybu/compute/cloudinit.py
+++ yaybu/compute/cloudinit.py
@@ -2,6 +2,7 @@
 import os
 import tempfile
 import subprocess
+import urllib2
 
 import logging
 
@@ -9,7 +10,7 @@ logger = logging.getLogger("cloudinit")
 
 class CloudInitException(Exception):
 
-    def __init__(self, message, log):
+    def __init__(self, message, log=""):
         self.message = message
         self.log = log
 
@@ -66,20 +67,62 @@ class Seed:
             os.unlink(os.path.join(self.tmpdir, f))
         os.rmdir(self.tmpdir)
 
+class RemoteUbuntuCloudImage:
+
+    server = "cloud-images.ubuntu.com"
+    source = "http://{server}/releases/{release}/release"
+    filename = "ubuntu-{release}-server-cloudimg-{arch}.{extension}"
+    blocksize = 81920
+
+    def __init__(self, directory, release="13.10", arch="amd64"):
+        """ Specify a local filename which will be overwritten if missing our out of date """
+        self.directory = directory
+        self.release = release
+        self.arch = arch
+
+    def remote_url(self, extension):
+        urlpattern = self.remote + "/" + self.filename
+
+    def fetch(self, extension):
+        args = dict(server=self.server, release=self.release, arch=self.arch, extension=extension)
+        source = self.source.format(**args)
+        filename = self.filename.format(**args)
+        remote_url = source + "/" + filename
+        try:
+            response = urllib2.urlopen(remote_url)
+        except urllib2.HTTPError:
+            raise CloudInitException("Unable to fetch {0}".format(remote_url))
+        local = open(os.path.join(self.directory, filename), "w")
+        while True:
+            data = response.read(self.blocksize)
+            if not data:
+                break
+            local.write(data)
+
 class CloudInit:
 
-    def __init__(self):
-        pass
+    def __init__(self, directory, release="13.10", arch="amd64"):
+        self.directory = directory
+        self.release = release
+        self.arch = arch
 
-    def create_seed(self, filename):
+    def create_seed(self):
         """ Create a seed ISO image in the specified filename """
+        s = Seed(os.path.join(self.directory, "seed.iso"))
+        s.update()
+        s.save()
+        s.cleanup()
+
+    def fetch_image(self):
+        r = RemoteUbuntuCloudImage(self.directory)
+        r.fetch("tar.gz")
+        r.fetch("ovf")
 
 
 if __name__ == "__main__":
-    s = Seed("seed.iso")
-    s.update()
-    s.save()
-    s.cleanup()
+    cloudinit = CloudInit(".")
+    cloudinit.create_seed()
+    cloudinit.fetch_image()
 
 # create the seed iso image
 # fetch the image from the ubuntu cloud website
