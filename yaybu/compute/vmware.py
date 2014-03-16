cd5658319ddefaad8d6f2ae8bea1210e7cc4864b
John Carr <john.carr@unrouted.co.uk>
2014-03-16 11:35:27 +0000

    Break out common code for subprocess handling

--- yaybu/compute/vmware.py
+++ yaybu/compute/vmware.py
@@ -22,17 +22,10 @@
 # 3. There needs to be a user with a password/key that can get to root
 # without sudo requiring a passphrase.
 
-#=========================================================================
-# libcloud/common/process.py
-
 import os
-import shlex
-import subprocess
-from pipes import quote
 
-from libcloud.common.types import LibcloudError, ProviderError
+from libcloud.common.types import LibcloudError
 from libcloud.compute.base import NodeAuthPassword, NodeAuthSSHKey
-import logging
 import json
 import urllib2
 import uuid
@@ -41,11 +34,17 @@ import urlparse
 import tempfile
 from functools import partial
 import time
+import shutil
+import hashlib
+
+from libcloud.compute.base import NodeDriver, Node
+from libcloud.compute.base import NodeState
+from libcloud.compute.types import Provider
+
 
 import zipfile
 from yaybu.util import ZipFile
-
-logger = logging.getLogger("yaybu.parts.compute.vmware")
+from yaybu.compute.process import Connection, Response
 
 
 class VMRunError(LibcloudError):
@@ -58,91 +57,17 @@ class FileAlreadyExistsError(VMRunError):
         self.value = "File or directory already exists"
 
 
-class Response(object):
-
-    def __init__(self, status, body, error):
-        self.status = status
-        self.body = body
-        self.error = error
-
-        if not self.success():
-            raise self.parse_error()
-
-        self.object = self.parse_body()
-
-    def parse_body(self):
-        return self.body
+class VMRunResponse(Response):
 
     def parse_error(self):
         if self.body == 'Error: The file already exists\n':
             raise FileAlreadyExistsError()
-        raise ProviderError(self.body + " " + self.error, self.error)
-
-    def success(self):
-        return self.status == 0
+        super(VMRunResponse, self).parse_error()
 
 
-class Connection(object):
+class VMRunConnection(Connection):
+    responseCls = VMRunResponse
 
-    responseCls = Response
-    log = None
-
-    def __init__(self, secure=True, host=None, port=None, url=None,
-                 timeout=None):
-        pass
-
-    def connect(self):
-        pass
-
-    def request(self, command, data='', capture_output=True):
-        if not isinstance(command, list):
-            command = shlex.split(command)
-
-        if self.log:
-            self.log.write(' '.join(quote(c) for c in command) + '\n')
-
-        if not capture_output:
-            stdout, stderr = '', ''
-            returncode = self._silent_request(command, data)
-        else:
-            returncode, stdout, stderr = self._request(command, data)
-
-        if self.log:
-            self.log.write("# returncode is %d\n" % returncode)
-            self.log.write("# -------- begin stdout ----------\n")
-            self.log.write(stdout)
-            self.log.write("# -------- begin stderr ----------\n")
-            self.log.write(stderr)
-            self.log.write("# -------- end ----------\n")
-
-        return self.responseCls(returncode, stdout, stderr)
-
-    def _request(self, command, data):
-        stdin = subprocess.PIPE if data else None
-        p = subprocess.Popen(
-            command, stdin=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate(data)
-        return p.returncode, stdout, stderr
-
-    def _silent_request(self, command, data):
-        stdin = subprocess.PIPE if data else None
-        with open(os.devnull, "w") as null:
-            p = subprocess.Popen(
-                command, stdin=stdin, stdout=null, stderr=null)
-            if data:
-                p.stdin.write(data)
-                p.stdin.close()
-            return p.wait()
-
-#=========================================================================
-
-import shutil
-import hashlib
-
-from libcloud.common.types import LibcloudError
-from libcloud.compute.base import NodeDriver, Node
-from libcloud.compute.base import NodeState
-from libcloud.compute.types import Provider
 
 # FIXME:
 Provider.VMWARE = 99
@@ -210,7 +135,7 @@ class VMWareDriver(NodeDriver):
     type = Provider.VMWARE
     name = "vmware"
     website = "http://www.vmware.com/products/fusion/"
-    connectionCls = Connection
+    connectionCls = VMRunConnection
     features = {'create_node': ['ssh_key', 'password']}
 
     def __init__(self, yaybu_root="~/.yaybu", vmrun=None, hosttype=None):
@@ -267,7 +192,6 @@ class VMWareDriver(NodeDriver):
     def _action(self, *params, **kwargs):
         capture_output = kwargs.get('capture_output', True)
         command = [self.vmrun, "-T", self.hosttype] + list(params)
-        logger.debug("Executing %r" % (" ".join(command),))
         return (
             self.connection.request(
                 command,
@@ -434,12 +358,10 @@ class VMWareDriver(NodeDriver):
         return node
 
     def reboot_node(self, node):
-        logger.debug("Rebooting node %r" % (node.id,))
         self._action("reset", node.id, "hard")
         node.state = NodeState.REBOOTING
 
     def destroy_node(self, node):
-        logger.debug("Destroying node %r" % (node.id,))
         self._action("stop", node.id, "hard")
         self._action("deleteVM", node.id)
         shutil.rmtree(os.path.dirname(node.id))
@@ -447,13 +369,9 @@ class VMWareDriver(NodeDriver):
     def ex_get_runtime_variable(self, node, variable):
         value = self._action(
             "readVariable", node.id, "runtimeConfig", variable)
-        logger.debug("Read variable %r from node %r, value was %r" %
-                     (variable, node.id, value))
         return value
 
     def ex_set_runtime_variable(self, node, variable, value):
-        logger.debug("Setting runtime variable %r on node %r to %r" %
-                     (variable, node.id, value))
         self._action(
             "writeVariable", node.id, "runtimeConfig", variable, value)
 
