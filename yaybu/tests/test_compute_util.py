dca68e9aecd2930956ddf029c9da212509021c57
Doug Winter <doug.winter@isotoma.com>
2014-03-25 10:17:39 +0000

    utility for locating and calling other commands

--- yaybu/tests/test_compute_util.py
+++ yaybu/tests/test_compute_util.py
@@ -2,28 +2,28 @@ import unittest2
 import mock
 from yaybu.compute import util
 
+
 class TestSubRunner(unittest2.TestCase):
 
-    @mock.patch("os.path.exists")
-    def setUp(self, m_exists):
-        m_exists.side_effect = lambda path: path == '/usr/local/doge/foo'
+    @mock.patch("os.path.isfile")
+    @mock.patch("os.access")
+    def _create(self, where, bail, m_access, m_isfile):
+        m_access.return_value = True
+        m_isfile.side_effect = lambda path: path == where
         with mock.patch.dict('os.environ', {'PATH': "/path/1:/path/2"}):
+            self.subrunner = util.SubRunner(
+                command_name="foo",
+                known_locations=["/opt/doesnotexist", "/usr/local/doge"],
+                bail_if_absent=bail,
+            )
 
-            class MockSubRunner(util.SubRunner):
+    def test_exists_bail(self):
+        self._create("/usr/local/doge/foo", True)
+        self.assertEqual(self.subrunner.pathname, "/usr/local/doge/foo")
 
-                @classmethod
-                def command_name(klass):
-                    return "foo"
+    def test_noexists_bail(self):
+        self.assertRaises(util.SubRunnerException, self._create, "", True)
 
-                @classmethod
-                def known_locations(klass):
-                    return [
-                        "/opt/doesnotexist",
-                        "/usr/local/doge",
-                    ]
-
-        self.subrunner = MockSubRunner()
-
-    def test_location(self):
-        self.assertEqual(self.subrunner.command_path, "/usr/local/doge/foo")
-
+    def test_noexists_nobail(self):
+        self._create("", False)
+        self.assertEqual(self.subrunner.pathname, None)
