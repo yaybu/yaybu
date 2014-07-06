9f7c45b56a003f1e293f09ed45f3d3a19e0714ad
John Carr <john.carr@unrouted.co.uk>
2014-07-06 13:13:20 +0100

    Remove old provisioner code

--- yaybu/provisioner/__init__.py
+++ yaybu/provisioner/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2013 Isotoma Limited
+# Copyright 2013-2014 Isotoma Limited
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pkg_resources import iter_entry_points
-
 from .part import Provision
-
-import yaybu.provisioner.resources  # NOQA
-for ep in iter_entry_points(group='yaybu.resource', name=None):
-    ep.load()
-
-import yaybu.provisioner.providers  # NOQA
-for ep in iter_entry_points(group='yaybu.providers', name=None):
-    ep.load()
 
 
 __all__ = [
--- yaybu/provisioner/part.py
+++ yaybu/provisioner/part.py
@@ -12,21 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import logging
 import getpass
-import urlparse
 
-import yay
-from yay.errors import NotFound, NotModified
-
-from yaybu.provisioner import resource
 from yaybu.changes import TextRenderer
-from yaybu import error
-from yaybu.error import MissingAsset, UnmodifiedAsset
 from yaybu import base
-
-from . import event, transports
 
 
 logger = logging.getLogger(__name__)
@@ -43,10 +33,6 @@ class Provision(base.GraphExternalAction):
 
         resources: {{ resources }}
     """
-
-    transports = {
-        "ssh": transports.SSHTransport,
-    }
 
     def apply(self):
         if self.root.readonly:
@@ -71,82 +57,23 @@ class Provision(base.GraphExternalAction):
         self.verbose = root.verbose
 
         self.options = {}
-        if os.path.exists("/etc/yaybu"):
-            self.options = yay.load_uri("/etc/yaybu")
-
-        if "://" in hostname:
-            parsed = urlparse.urlparse(hostname)
-            Transport = self.transports[parsed.scheme]
-        else:
-            Transport = self.transports["ssh"]
-
-        self.transport = Transport(
-            context=self,
-            verbose=root.verbose,
-            simulate=root.simulate,
-        )
 
         with root.ui.throbber("Connect to '%s'" % hostname):
             self.transport.connect()
 
-        if not self.simulate and not self.transport.exists(self.get_data_path()):
-            self.transport.makedirs(self.get_data_path())
-
-        # This makes me a little sad inside, but the whole
-        # context thing needs a little thought before jumping in
-        self.state = event.EventState()
-        self.state.save_file = self.get_data_path("events.saved")
-        self.state.simulate = self.simulate
-        self.state.transport = self.transport
-
-        if not self.simulate:
-            save_parent = os.path.realpath(
-                os.path.join(self.state.save_file, os.path.pardir))
-            if not self.transport.exists(save_parent):
-                self.transport.makedirs(save_parent)
-
-        if self.transport.exists(self.state.save_file):
-            if self.resume:
-                self.state.loaded = False
-            elif self.no_resume:
-                if not self.simulate:
-                    self.transport.unlink(self.state.save_file)
-                self.state.loaded = True
-            else:
-                raise error.SavedEventsAndNoInstruction(
-                    "There is a saved events file - you need to specify --resume or --no-resume")
-
         # Actually apply the configuration
-        bundle = resource.ResourceBundle.create_from_yay_expression(
-            self.params.resources, verbose_errors=self.verbose > 2)
-        bundle.bind()
+        #bundle = ResourceBundle.create_from_yay_expression(
+        #    self.params.resources, verbose_errors=self.verbose > 2)
 
-        with self.root.ui.throbber("Provision %s" % self.host) as throbber:
-            changed = bundle.apply(self, throbber)
-        self.root.changed(changed)
-
-        if not self.simulate and self.transport.exists(self.state.save_file):
-            self.transport.unlink(self.state.save_file)
+        #with self.root.ui.throbber("Provision %s" % self.host) as throbber:
+        #    changed = bundle.apply(self, throbber)
+        #self.root.changed(changed)
 
     def test(self):
-        bundle = resource.ResourceBundle.create_from_yay_expression(
-            self.params.resources)
-        bundle.bind()
-        bundle.test(self)
+        #bundle = ResourceBundle.create_from_yay_expression(
+        #    self.params.resources)
+        pass
 
     def change(self, change):
         renderer = TextRenderer.get(change, self.current_output)
         return change.apply(self, renderer)
-
-    def get_file(self, filename, etag=None):
-        try:
-            return self.root.openers.open(filename, etag)
-        except NotModified as e:
-            raise UnmodifiedAsset(str(e))
-        except NotFound as e:
-            raise MissingAsset(str(e))
-
-    def get_data_path(self, path=None):
-        if not path:
-            return "/var/run/yaybu"
-        return os.path.join("/var/run/yaybu", path)
